8f1092e26778f254b3daa578ed11c5dbdfb0744d
fumyskin <vzando04@gmail.com>
2025-11-09 23:40:05 +0100

    Added new siulation graphs

--- DELIVERABLE1_CODE/compile.sh
+++ DELIVERABLE1_CODE/compile.sh
@@ -2,10 +2,10 @@
 # let's see where this goes...
 
 # Configuration
-MATRIX="1138_bus/1138_bus.mtx"
+MATRIX=("1138_bus/1138_bus.mtx" "utm5940/utm5940.mtx")
 RUNS=12
 THREADS=(1 2 4 8 16 32 64)  # adjust for your system
-EXECUTABLES=("spmv_static" "spmv_dynamic" "spmv_guided" "spmv_manual")
+EXECUTABLES=("spmv_sequential" "spmv_static" "spmv_manual")
 CHUNK_SIZE=(10 100 1000)
 
 # others to add: "spmv_collapse" "spmv_runtime" "spmv_auto" "spmv_chunked"!!!!!!!!!!!
@@ -15,21 +15,24 @@ OUTPUT_FILE="results.csv"
 echo "Executable,Threads,Run,ChunkSize,Time" > "$OUTPUT_FILE"
 
 # Loop for each executable
-for EXEC in "${EXECUTABLES[@]}"; do
-    for T in "${THREADS[@]}"; do
-        echo "Running $EXEC with $T threads..."
-        export OMP_NUM_THREADS=$T
+for MATRIX in "${MATRIX[@]}"; do
+    echo "Testing matrix: $MATRIX ..."
+    for EXEC in "${EXECUTABLES[@]}"; do
+        for T in "${THREADS[@]}"; do
+            echo "Running $EXEC with $T threads..."
+            export OMP_NUM_THREADS=$T
 
-        for ((i=1; i<=RUNS; i++)); do
-            if [ "$EXEC" == "spmv_chunked" ]; then
-                for CHUNK in "${CHUNK_SIZE[@]}"; do
-                    output=$(./"$EXEC" "$MATRIX" "$CHUNK" | grep "Elapsed time" | awk '{print $3}')
-                    echo "$EXEC,$T,$i,$CHUNK,$output" >> "$OUTPUT_FILE"
-                done
-            else
-                output=$(./"$EXEC" "$MATRIX" | grep "Elapsed time" | awk '{print $3}')
-                echo "$EXEC,$T,$i,NA,$output" >> "$OUTPUT_FILE"
-            fi
+            for ((i=1; i<=RUNS; i++)); do
+                if [ "$EXEC" == "spmv_chunked" ]; then
+                    for CHUNK in "${CHUNK_SIZE[@]}"; do
+                        output=$(./"$EXEC" "$MATRIX" "$CHUNK" | grep "Elapsed time" | awk '{print $3}')
+                        echo "$EXEC,$T,$i,$CHUNK,$output" >> "$OUTPUT_FILE"
+                    done
+                else
+                    output=$(./"$EXEC" "$MATRIX" | grep "Elapsed time" | awk '{print $3}')
+                    echo "$EXEC,$T,$i,NA,$output" >> "$OUTPUT_FILE"
+                fi
+            done
         done
     done
 done
